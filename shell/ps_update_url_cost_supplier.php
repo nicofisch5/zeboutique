ff35c2db6a0ff6f9513aef2ba35ca6e3f012b123
Nicolas Fischer <nicofisch5@gmail.com>
2013-04-09 15:03:33 +0200

    Shell

---
+++ shell/ps_update_url_cost_supplier.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Shell
+ * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+require_once 'abstract.php';
+
+/**
+ * Magento Compiler Shell Script
+ *
+ * @category    Mage
+ * @package     Mage_Shell
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Zemode_Shell_StarnetUpdateSku extends Mage_Shell_Abstract
+{
+    
+    const FILENAME = 'ps_product.csv';
+    
+    protected $_attributeOptions = array();
+    
+    /**
+     * Get CSV stream
+     * 
+     * @return Varien_Io_File
+     */
+    protected function _getCsvStream()
+    {
+        $io     = new Varien_Io_File();
+        $info   = pathinfo(self::FILENAME);
+        $io->open(array('path' => $info['dirname']));
+        $io->streamOpen($info['basename'], 'r');
+
+        // Skip headers
+        $headers = $io->streamReadCsv();
+        
+        return $io;
+    }
+    
+    /**
+     * Get Connection object
+     *
+     * @return Object
+     */
+    protected function _getConnection()
+    {
+        return Mage::getSingleton('core/resource')->getConnection('catalog_read');
+    }
+
+    /**
+     * Get Select object
+     *
+     * @return Object
+     */
+    protected function _getSelect()
+    {
+        return $this->_getConnection()->select();
+    }
+    
+    /**
+     * Get Table
+     *
+     * @param string $table
+     * @return string
+     */
+    protected function _getTable($table)
+    {
+        return Mage::getSingleton('core/resource')->getTableName($table);
+    }
+    
+    /**
+     * Get Attribute Options
+     *
+     * @param string $attCode
+     * @return array
+     */
+    protected function _getAttributeOptions($attCode)
+    {
+        if (! array_key_exists($attCode, $this->_attributeOptions)) {
+            $select = $this->_getSelect()
+                ->from(array('a'=>$this->_getTable('eav/attribute')))
+                ->join(array('ao'=>$this->_getTable('eav/attribute_option')), 'ao.attribute_id=a.attribute_id', array('ao.option_id'))
+                ->where('a.attribute_code = (?)', $attCode)
+                ->joinLeft(
+                    array('store'=>$this->_getTable('eav/attribute_option_value')),
+                    "store.option_id=ao.option_id and store.store_id=0",
+                    array('value'=>"store.value")
+                );
+
+            $rawOptions = $this->_getConnection()->fetchAll($select);
+            $options = array();
+            foreach ($rawOptions as $rawOption) {
+                $options[$rawOption['value']] = $rawOption['option_id'];
+            }
+            $this->_attributeOptions[$attCode] = $options;
+        }
+
+        return $this->_attributeOptions[$attCode];
+    }
+    
+    /**
+     * Get Attribute Option Id By Value
+     *
+     * @param string $attCode
+     * @param string $value
+     * @return int
+     */
+    protected function _getAttributeOptionIdByValue($attCode, $value)
+    {
+        $options = $this->_getAttributeOptions($attCode);
+        return $options[$value];
+    }
+    
+    /**
+     * Try to update Magen,to SKU with starnet csv file
+     * 
+     * @return null
+     */
+    public function run()
+    {        
+        $this->_updatedRows = 0;
+        
+        $io = $this->_getCsvStream();
+        
+        try {
+            $rowNumber  = 1;
+            $importData = array();
+
+            while (false !== ($csvLine = $io->streamReadCsv(","))) {
+                $rowNumber++;
+
+                if (empty($csvLine)) {
+                    continue;
+                }
+                
+                // Empay SKU
+                if (! isset($csvLine[0])) {
+                    continue;
+                }
+
+                // Load product by SKU
+                $prd = Mage::getModel('catalog/product');
+                $prd->load($prd->getIdBySku($csvLine[0]));
+                
+                if (! $prd->getId()) {
+                    Mage::log('SKU '.$csvLine[0].' non trouvée dans Magento');
+                    continue;
+                }
+                
+                $csvLine[3] = ucfirst($csvLine[3]);
+                
+                // Check type ID
+                if ($prd->getData('type_id') == 'configurable') {
+                    // If configurable we load all associated simples
+                    $childIds = $prd->getTypeInstance()->getChildrenIds($prd->getId());
+                    
+                    // Parse all child
+                    foreach ($childIds[0] as $childId) {
+                        $prdChild = Mage::getModel('catalog/product')->load($childId);
+                        $prdChild->setData('cost', $csvLine[2])
+                                ->setData('supplier', $this->_getAttributeOptionIdByValue('supplier', $csvLine[3]))
+                                ->setData('price', $csvLine[4])
+                                ->save();
+                        Mage::log('SKU '.$prdChild->getData('sku'). ' (simple) mis à jour (cost, supplier, price)');
+                    }
+                    
+                }
+                
+                // Update data
+                $prd->setData('ps_url', $csvLine[1])
+                     ->setData('cost', $csvLine[2])
+                     ->setData('supplier', $this->_getAttributeOptionIdByValue('supplier', $csvLine[3]))
+                     ->setData('price', $csvLine[4])
+                     ->save();
+                Mage::log('SKU '.$csvLine[0]. ' (configurable) mis à jour (ps_url, cost, supplier, price)');
+
+                $this->_updatedRows++;
+            }
+        } catch (Mage_Core_Exception $e) {
+            $io->streamClose();
+            echo 'Erreur : '.$e->getMessage();
+        } catch (Exception $e) {
+            $io->streamClose();
+            Mage::logException($e);
+            echo 'Erreur : '.$e->getMessage();
+        }
+
+        echo "\n\n Nb lignes mises à jour : ".$this->_updatedRows."\n\n";
+        
+        return $this;
+    }
+}
+
+$shell = new Zemode_Shell_StarnetUpdateSku();
+$shell->run();
---
+++ shell/starnet_update_sku.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Shell
+ * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+require_once 'abstract.php';
+
+/**
+ * Magento Compiler Shell Script
+ *
+ * @category    Mage
+ * @package     Mage_Shell
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Zemode_Shell_StarnetUpdateSku extends Mage_Shell_Abstract
+{
+    
+    const FILENAME = 'stock_starnet.csv';
+    const URL_STOCK_FILE = 'http://www.starnet-world.com/stock_starnet.csv';
+    const URL_TARIF_FILE = 'http://www.starnet-world.com/files/tarifs_clients_2013.xlsx';
+    const STARNET_OPTION_ID = 311;
+    
+    // C2c_Couleur_Taille_pap
+    //protected $_attSet = 10; protected $_attArray = array('c2c_taille_pap', 'c2c_couleur_pap');
+    
+    // C2c_Couleur_pap
+    //protected $_attSet = 14; protected $_attArray = array('c2c_couleur_pap');
+    
+    // C2c_Taille_pap
+    //protected $_attSet = 16; protected $_attArray = array('c2c_taille_pap');
+    
+    // C2c_Couleur
+    //protected $_attSet = 20; protected $_attArray = array('c2c_couleur');
+    
+    // C2c_Couleur_Taille
+    protected $_attSet = 26;
+    
+    protected $_taille = array(
+        'ps' => 'Grande Taille Unique',
+        'l' => 'L-40',
+        'lxl' => 'L/XL',
+        'os' => 'Taille Unique',
+        'XXXXL-48',
+        'm' => 'M-38',
+        'ml' => 'M/L',
+        's' => 'S-36',
+        'sm' => 'S/M',
+        'xl' => 'XL-42',
+        'xs' => 'XS-34',
+        'xxl' => 'XXL-44',
+        'xxlxxxl' => 'XXXL-46'
+    );
+  
+    /**
+     * Get CSV stream
+     * 
+     * @return Varien_Io_File
+     */
+    protected function _getCsvStream()
+    {
+        $io     = new Varien_Io_File();
+        $info   = pathinfo(self::FILENAME);
+        $io->open(array('path' => $info['dirname']));
+        $io->streamOpen($info['basename'], 'r');
+
+        // Skip headers
+        $headers = $io->streamReadCsv();
+        
+        return $io;
+    }
+    
+    /**
+     * Try to update Magen,to SKU with starnet csv file
+     * 
+     * @return null
+     */
+    public function run()
+    {
+        $this->_updatedRows = 0;
+        
+        $io = $this->_getCsvStream();
+        
+        try {
+            $rowNumber  = 1;
+            $importData = array();
+
+            while (false !== ($csvLine = $io->streamReadCsv("\t"))) {
+                $rowNumber++;
+
+                if (empty($csvLine)) {
+                    continue;
+                }
+
+                /**
+                 * 1. Load tout les produits simples dont le SKU commence par la référence maître
+                 * 2. Pour chacun des produits de la collection on essaye de trouver l'intitulé de
+                 * la couleur Magento dans l'intitulé starnet
+				 *	- si on trouve on met à jour la référence.
+                 *   - sinon on loggue
+                */
+                
+                // Products that begin with starnet SKU master
+                $coll = Mage::getModel('catalog/product')->getCollection()
+                            ->addFieldToFilter('type_id', 'simple')
+                            ->addFieldToFilter('sku', array('like'=>$csvLine[0].'%'))
+                            ->addFieldToFilter('attribute_set_id', $this->_attSet);
+
+                // Skip line if no product in Magento
+                if ($coll->getSize() == 0) {
+                    continue;
+                }
+
+                // Parse products collection
+                foreach ($coll as $prd) {
+                    $prd->load($prd->getId());
+
+                    // Search color label in strnet product label
+                    //$starnetLabel = strtolower($csvLine[2]);
+
+                    // Regex to find color and size
+                    preg_match('@couleur.?:.?(.*) \/.?taille.?:.?(.*)@i', $csvLine[2], $matches);
+                    $couleur = strtolower($matches[1]);
+                    $taille = strtolower($matches[2]);
+
+                    //preg_match('@couleur.?:.?(.*)@i', $csvLine[2], $matches);
+                    //$couleur = strtolower($matches[1]);
+                    
+                        $prdCouleur = strtolower($prd->getAttributeText('c2c_couleur'));
+                        $prdTaille = $prd->getAttributeText('c2c_taille');
+                        if (! $prdCouleur || ! $prdTaille) {
+                            continue;
+                        }
+                        
+                        // Check couleur
+                        if (strstr($couleur, $prdCouleur) === false) {
+                            continue;
+                        }
+                        
+                        // Check taille
+                        if (! array_key_exists($taille, $this->_taille)) {
+                            echo 'Taille non trouvée';
+                            continue;
+                        }
+                        
+                        if ($this->_taille[$taille] != $prdTaille) {
+                            continue;
+                        }
+                    
+                    // Update SKU
+                    $this->_updatedRows++;
+                    //$msg = $csvLine[2]." - $prdCouleur - $prdTaille - (".$prd->getId().") \n";
+                    $msg = $csvLine[2]." - $prdCouleur - (".$prd->getId().") \n";
+                    echo $msg;
+                    /*Mage::log($msg);
+                    $prd->setData('sku', $csvLine[1])
+                        ->setData('starnet_sku_updated', 1)
+                        ->setData('supplier', self::STARNET_OPTION_ID)
+                        ->save();*/
+                }
+            }
+        } catch (Mage_Core_Exception $e) {
+            $adapter->rollback();
+            $io->streamClose();
+            echo 'Erreur : '.$e->getMessage();
+        } catch (Exception $e) {
+            $adapter->rollback();
+            $io->streamClose();
+            Mage::logException($e);
+            echo 'Erreur : '.$e->getMessage();
+        }
+
+        echo "\n\n Nb lignes mises à jour : ".$this->_updatedRows."\n\n";
+        
+        return $this;
+    }
+}
+
+$shell = new Zemode_Shell_StarnetUpdateSku();
+$shell->run();
