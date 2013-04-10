8945ec36feaafed8ebe6122401ec729d5dfedd82
Nicolas Fischer <nicofisch5@gmail.com>
2013-04-10 14:30:16 +0200

    Evol grille produit

---
+++ app/code/local/Zeboutique/Adminhtml/Block/Catalog/Product/Grid.php
@@ -0,0 +1,299 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Zeboutique
+ * @package     Zeboutique_Adminhtml
+ * @copyright   Copyright (c) 2013 Zeboutique
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author      Zeboutique
+ */
+
+/**
+ * Zeboutique Starnet
+ *
+ * @category    Zeboutique
+ * @package     Zeboutique_Adminhtml
+ * @author      Zeboutique
+ */
+class Zeboutique_Adminhtml_Block_Catalog_Product_Grid extends Mage_Adminhtml_Block_Catalog_Product_Grid
+{
+
+    /**
+     * Prepare collection 
+     * 
+     * @return Zeboutique_Adminhtml_Block_Catalog_Product_Grid
+    */
+    protected function _prepareCollection()
+    {
+        $store = $this->_getStore();
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('attribute_set_id')
+            ->addAttributeToSelect('type_id')
+            ->addAttributeToSelect('supplier');
+
+        if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
+            $collection->joinField('qty',
+                'cataloginventory/stock_item',
+                'qty',
+                'product_id=entity_id',
+                '{{table}}.stock_id=1',
+                'left');
+        }
+        if ($store->getId()) {
+            //$collection->setStoreId($store->getId());
+            $adminStore = Mage_Core_Model_App::ADMIN_STORE_ID;
+            $collection->addStoreFilter($store);
+            $collection->joinAttribute(
+                'name',
+                'catalog_product/name',
+                'entity_id',
+                null,
+                'inner',
+                $adminStore
+            );
+            $collection->joinAttribute(
+                'custom_name',
+                'catalog_product/name',
+                'entity_id',
+                null,
+                'inner',
+                $store->getId()
+            );
+            $collection->joinAttribute(
+                'status',
+                'catalog_product/status',
+                'entity_id',
+                null,
+                'inner',
+                $store->getId()
+            );
+            $collection->joinAttribute(
+                'visibility',
+                'catalog_product/visibility',
+                'entity_id',
+                null,
+                'inner',
+                $store->getId()
+            );
+            $collection->joinAttribute(
+                'price',
+                'catalog_product/price',
+                'entity_id',
+                null,
+                'left',
+                $store->getId()
+            );
+        }
+        else {
+            $collection->addAttributeToSelect('price');
+            $collection->joinAttribute('status', 'catalog_product/status', 'entity_id', null, 'inner');
+            $collection->joinAttribute('visibility', 'catalog_product/visibility', 'entity_id', null, 'inner');
+        }
+
+        $this->setCollection($collection);
+
+        Mage_Adminhtml_Block_Widget_Grid::_prepareCollection();
+        $this->getCollection()->addWebsiteNamesToResult();
+        return $this;
+    }
+    
+    /**
+     * Prepare columns 
+     * 
+     * @return Zeboutique_Adminhtml_Block_Catalog_Product_Grid
+    */
+    protected function _prepareColumns()
+    {
+        $this->addColumn('entity_id',
+            array(
+                'header'=> Mage::helper('catalog')->__('ID'),
+                'width' => '50px',
+                'type'  => 'number',
+                'index' => 'entity_id',
+        ));
+        $this->addColumn('name',
+            array(
+                'header'=> Mage::helper('catalog')->__('Name'),
+                'index' => 'name',
+        ));
+
+        $store = $this->_getStore();
+        if ($store->getId()) {
+            $this->addColumn('custom_name',
+                array(
+                    'header'=> Mage::helper('catalog')->__('Name in %s', $store->getName()),
+                    'index' => 'custom_name',
+            ));
+        }
+
+        $this->addColumn('type',
+            array(
+                'header'=> Mage::helper('catalog')->__('Type'),
+                'width' => '60px',
+                'index' => 'type_id',
+                'type'  => 'options',
+                'options' => Mage::getSingleton('catalog/product_type')->getOptionArray(),
+        ));
+
+        $sets = Mage::getResourceModel('eav/entity_attribute_set_collection')
+            ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
+            ->load()
+            ->toOptionHash();
+
+        $this->addColumn('set_name',
+            array(
+                'header'=> Mage::helper('catalog')->__('Attrib. Set Name'),
+                'width' => '100px',
+                'index' => 'attribute_set_id',
+                'type'  => 'options',
+                'options' => $sets,
+        ));
+
+        $this->addColumn('sku',
+            array(
+                'header'=> Mage::helper('catalog')->__('SKU'),
+                'width' => '80px',
+                'index' => 'sku',
+        ));
+
+        $store = $this->_getStore();
+        $this->addColumn('price',
+            array(
+                'header'=> Mage::helper('catalog')->__('Price'),
+                'type'  => 'price',
+                'currency_code' => $store->getBaseCurrency()->getCode(),
+                'index' => 'price',
+        ));
+
+        if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
+            $this->addColumn('qty',
+                array(
+                    'header'=> Mage::helper('catalog')->__('Qty'),
+                    'width' => '100px',
+                    'type'  => 'number',
+                    'index' => 'qty',
+            ));
+        }
+        
+        // Manufacturer
+        $this->addColumn('manufacturer',
+            array(
+                'header'=> Mage::helper('catalog')->__('Manufacturer'),
+                'index' => 'manufacturer',
+                'type'  => 'options',
+                'options' => $this->_getColumnOptions('manufacturer'),
+        ));
+        
+        // Supplier
+        $this->addColumn('supplier',
+            array(
+                'header'=> Mage::helper('catalog')->__('Supplier'),
+                'index' => 'supplier',
+                'type'  => 'options',
+                'options' => $this->_getColumnOptions('supplier'),
+        ));
+
+        $this->addColumn('visibility',
+            array(
+                'header'=> Mage::helper('catalog')->__('Visibility'),
+                'width' => '70px',
+                'index' => 'visibility',
+                'type'  => 'options',
+                'options' => Mage::getModel('catalog/product_visibility')->getOptionArray(),
+        ));
+
+        $this->addColumn('status',
+            array(
+                'header'=> Mage::helper('catalog')->__('Status'),
+                'width' => '70px',
+                'index' => 'status',
+                'type'  => 'options',
+                'options' => Mage::getSingleton('catalog/product_status')->getOptionArray(),
+        ));
+
+        if (!Mage::app()->isSingleStoreMode()) {
+            $this->addColumn('websites',
+                array(
+                    'header'=> Mage::helper('catalog')->__('Websites'),
+                    'width' => '100px',
+                    'sortable'  => false,
+                    'index'     => 'websites',
+                    'type'      => 'options',
+                    'options'   => Mage::getModel('core/website')->getCollection()->toOptionHash(),
+            ));
+        }
+
+        $this->addColumn('action',
+            array(
+                'header'    => Mage::helper('catalog')->__('Action'),
+                'width'     => '50px',
+                'type'      => 'action',
+                'getter'     => 'getId',
+                'actions'   => array(
+                    array(
+                        'caption' => Mage::helper('catalog')->__('Edit'),
+                        'url'     => array(
+                            'base'=>'*/*/edit',
+                            'params'=>array('store'=>$this->getRequest()->getParam('store'))
+                        ),
+                        'field'   => 'id'
+                    )
+                ),
+                'filter'    => false,
+                'sortable'  => false,
+                'index'     => 'stores',
+        ));
+
+        if (Mage::helper('catalog')->isModuleEnabled('Mage_Rss')) {
+            $this->addRssList('rss/catalog/notifystock', Mage::helper('catalog')->__('Notify Low Stock RSS'));
+        }
+
+        return Mage_Adminhtml_Block_Widget_Grid::_prepareColumns();
+    }
+    
+    /**
+     * Get options for an attribute
+     * 
+     * @param string $attCode
+     * @return array
+    */
+    protected function _getColumnOptions($attCode)
+    {
+        $attr = Mage::getModel('eav/entity_attribute')->loadByCode('catalog_product', $attCode);
+        $options = array();
+
+        if ($attr->getFrontendInput() == 'select') { 
+            $values = Mage::getResourceModel( 'eav/entity_attribute_option_collection' )->setAttributeFilter( 
+                $attr->getId() )
+                ->setStoreFilter( $this->_getStore()
+                ->getId(), false )
+                ->load();
+
+            $options = array();
+            foreach ($values as $value) {
+                $options[$value->getOptionId()] = $value->getValue();
+            }
+        }
+    
+        return $options;
+    }
+}
