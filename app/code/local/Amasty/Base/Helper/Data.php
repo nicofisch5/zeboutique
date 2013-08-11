42e922e81b058e9c595c6def1510ffda0657c007
Nicolas Fischer <nicofisch5@gmail.com>
2013-08-11 12:08:15 +0200

    Amasty

--- app/code/local/Amasty/Base/Helper/Data.php
+++ app/code/local/Amasty/Base/Helper/Data.php
@@ -4,4 +4,20 @@
  */  
 class Amasty_Base_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function isVersionLessThan($major=1, $minor=4)
+    {
+        $curr = explode('.', Mage::getVersion()); // 1.3. compatibility
+        $need = func_get_args();
+        foreach ($need as $k => $v){
+            if ($curr[$k] != $v)
+                return ($curr[$k] < $v);
+        }
+        return false;
+    } 
+    
+    public function isModuleActive($code)
+    {
+        return ('true' == (string)Mage::getConfig()->getNode('modules/'.$code.'/active'));
+    } 
+    
 }
