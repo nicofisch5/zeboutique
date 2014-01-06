c652f70ae90e46538b942ef773ff3c482c378891
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2014-01-06 11:12:04 +0100

    Module Productcontact

---
+++ app/code/local/Magestore/Productcontact/Block/Productcontact.php
@@ -0,0 +1,27 @@
+<?php
+class Magestore_Productcontact_Block_Productcontact extends Mage_Directory_Block_Data
+{
+	public function _prepareLayout()
+    {
+		return parent::_prepareLayout();
+    }
+    
+     public function getProductcontact()     
+     { 
+        if (!$this->hasData('productcontact')) {
+            $this->setData('productcontact', Mage::registry('productcontact'));
+        }
+        return $this->getData('productcontact');
+        
+    }
+	
+	public function getCustomer() {
+		return Mage::getSingleton('customer/session')->getCustomer();
+	}
+	
+	public function getTemplate()
+	{
+		return parent::getTemplate();
+	}	
+	
+}
